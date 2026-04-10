909fb421d780b960772db263c1a482f592b9dc2a
murilo-a-souza <sir.murilos@gmail.com>
2026-04-10 12:10:54 -0300

    Criação da Classe.

---
+++ exercicios/ExerciciosAula10/src/br/com/fiap/bean/Funcionario.java
@@ -0,0 +1,29 @@
+package br.com.fiap.bean;
+
+public class Funcionario {
+    private String nome;
+    private float valorHoraTrabalho;
+    public Funcionario() {
+    }
+    public Funcionario(String nome, float valorHoraTrabalho) {
+        this.nome = nome;
+        this.valorHoraTrabalho = valorHoraTrabalho;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getValorHoraTrabalho() {
+        return valorHoraTrabalho;
+    }
+    public void setValorHoraTrabalho(float valorHoraTrabalho) {
+        this.valorHoraTrabalho = valorHoraTrabalho;
+    }
+    public float calcularSalario(float qtdeHorasTrabalhoSemana) {
+        float salario = ( valorHoraTrabalho * qtdeHorasTrabalhoSemana ) * 4;
+        return salario;
+    }
+}
+
