128e7972b3b38a4d2ff02b9068f1a45886ce5349
murilo-a-souza <sir.murilos@gmail.com>
2026-05-07 11:10:21 -0300

    Aula 16 : Otimização e mudanças no código do Praticando 15.

--- exercicios/ExerciciosAula15/src/br/com/fiap/main/Main.java
+++ exercicios/ExerciciosAula15/src/br/com/fiap/main/Main.java
@@ -8,43 +8,39 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String auxiliar, nome, escolha = "sim";
-        float valorHoraTrabalho, adicionais;
-        int opcao;
+        String auxiliar, nome, exibe = ".", escolha = "sim";
+        float valorHoraTrabalho, adicionais, salario;
         while (escolha.equalsIgnoreCase("sim")) {
             try {
                 auxiliar = JOptionPane.showInputDialog("Qual tipo de salário deseja calcular?\n1. Funcionário (comum)\n2. Vigia noturno\n3. Vendedor");
-                opcao = Integer.parseInt(auxiliar);
+                int opcao = Integer.parseInt(auxiliar);
+                nome = JOptionPane.showInputDialog("Digite o nome do funcionário: ");
+                auxiliar = JOptionPane.showInputDialog(String.format("Digite o valor da hora de trabalho do funcionário %s: ", nome));
+                valorHoraTrabalho = Integer.parseInt(auxiliar);
                 switch (opcao) {
                     case 1:
-                        nome = JOptionPane.showInputDialog("Digite o nome do funcionário: ");
-                        auxiliar = JOptionPane.showInputDialog(String.format("Digite o valor da hora de trabalho do funcionário %s: ", nome));
-                        valorHoraTrabalho = Integer.parseInt(auxiliar);
                         Funcionario funcionario = new Funcionario(nome, valorHoraTrabalho);
+                        salario = funcionario.calcularSalario();
                         JOptionPane.showMessageDialog(null, String.format("O salário de %s é R$%.2f", nome, funcionario.calcularSalario()));
                         break;
                     case 2:
-                        nome = JOptionPane.showInputDialog("Digite o nome do vigia: ");
-                        auxiliar = JOptionPane.showInputDialog(String.format("Digite o valor da hora de trabalho do vigia %s: ", nome));
-                        valorHoraTrabalho = Integer.parseInt(auxiliar);
                         auxiliar = JOptionPane.showInputDialog(String.format("Digite o valor adicional recebido pelo vigia %s", nome));
                         adicionais = Integer.parseInt(auxiliar);
                         VigiaNoturno vigiaNoturno = new VigiaNoturno(nome, valorHoraTrabalho, adicionais);
-                        JOptionPane.showMessageDialog(null, String.format("O salário de %s é R$%.2f", nome, vigiaNoturno.calcularSalario()));
+                        salario = vigiaNoturno.calcularSalario();
+                        exibe = String.format("Nome: %s \nSalário: R$%.2f",vigiaNoturno.getNome(), salario);
                         break;
                     case 3:
-                        nome = JOptionPane.showInputDialog("Digite o nome do vendedor: ");
-                        auxiliar = JOptionPane.showInputDialog(String.format("Digite o valor da hora de trabalho do vendedor %s: ", nome));
-                        valorHoraTrabalho = Integer.parseInt(auxiliar);
                         auxiliar = JOptionPane.showInputDialog(String.format("Digite a comissão recebida pelo vendedor %s (%%)", nome));
                         adicionais = Integer.parseInt(auxiliar);
                         Vendedor vendedor = new Vendedor(nome, valorHoraTrabalho, adicionais);
-
-                        JOptionPane.showMessageDialog(null, String.format("O salário de %s é R$%.2f", nome, vendedor.calcularSalario()));
+                        salario = vendedor.calcularSalario();
+                        exibe = String.format("Nome: %s \nSalário: R$%.2f",vendedor.getNome(), salario);
                         break;
                     default:
-                        JOptionPane.showMessageDialog(null, "Digite apenas opções válidas! (1-3)");
+                        exibe = "Opção inválida";
                 }
+                JOptionPane.showMessageDialog(null, exibe);
                 escolha = JOptionPane.showInputDialog("Deseja continuar e realizar outro calculo de salário? sim/n");
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e.getMessage());
